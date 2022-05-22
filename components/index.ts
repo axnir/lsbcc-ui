008fcd9c1cce655a4aaaafc5020aca8b7ab7e894
axnir <lxgjzyj@hotmail.com>
2022-05-22 14:46:35 +0800

    feat: add Image component

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
 export { default as Alert } from './alert';
 export { default as Pagination } from './pagination';
 export { default as Popover} from './popover';
+export { default as Image} from './image';
